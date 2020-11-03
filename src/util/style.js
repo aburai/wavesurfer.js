152f1611d17485e5ab63f74e8f152a31d4b4bc5e
Andre Bunse <andre.bunse@gmail.com>
2020-11-03 21:02:57 +0100

    prevent errors when wavesurfer already destroyed

--- src/util/style.js
+++ src/util/style.js
@@ -7,6 +7,7 @@
  * @return {HTMLElement} el
  */
 export default function style(el, styles) {
+    if (!el) return el;
     Object.keys(styles).forEach(prop => {
         if (el.style[prop] !== styles[prop]) {
             el.style[prop] = styles[prop];
